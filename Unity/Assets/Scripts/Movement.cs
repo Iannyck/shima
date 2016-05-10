ded132f081d48ac8748f9fa097b532a6282d2c5d
eboucher4 <eric_2212@hotmail.ca>
2016-05-10 15:11:05 -0400

    Door
    Opening door and textures

---
+++ Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+}
+
+ 
+
