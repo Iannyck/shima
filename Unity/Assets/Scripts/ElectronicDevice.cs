5b58d8ef4b798b02dcc87aeea3f16250cb76bf79
Yannick <yannick.francillette1@uqac.ca>
2016-05-16 10:30:03 -0400

    Adds electronic device script
    Adds electronic device script

---
+++ Unity/Assets/Scripts/ElectronicDevice.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectronicDevice : MonoBehaviour {
+
+	public int phase;
+	public int power;
+
+	private GameObject electronicPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
