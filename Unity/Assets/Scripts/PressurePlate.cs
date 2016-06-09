4ddc8c375cc1b197ada97b37bda1e5f8458ee85f
eboucher4 <eric_2212@hotmail.ca>
2016-06-09 13:24:05 -0400

    Pressure Plate + RFID
    Pressure plate prototype

---
+++ Unity/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+
+    Rigidbody rgbd;
+
+    float massColliderEnter;
+    float massColliderExit;
+    float massTotale;
+
+    bool pressureOn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(massTotale);
+        Debug.Log(pressureOn);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        rgbd = other.GetComponent<Rigidbody>();
+        massColliderEnter = rgbd.mass;
+
+        massTotale = massTotale + massColliderEnter;
+        
+        if (massTotale != 0)
+        {
+            pressureOn = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        massColliderExit = other.GetComponent<Rigidbody>().mass;
+        massTotale = massTotale - massColliderExit;
+
+        if (massTotale == 0)
+        {
+            pressureOn = false;
+        }
+
+    }
+}
