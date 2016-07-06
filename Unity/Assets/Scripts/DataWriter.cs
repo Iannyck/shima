f79023d32ad176c5aed558475404dd1c79e3d1e7
Yannick <yannick.francillette1@uqac.ca>
2016-07-06 19:40:38 -0400

    Adds database prototype scripts
    Adds database prototype scripts

---
+++ Unity/Assets/Scripts/DataWriter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataWriter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Write(string id, Hashtable data) {
+	}
+}
