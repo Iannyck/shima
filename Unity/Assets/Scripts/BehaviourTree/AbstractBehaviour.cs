5e954fa4175f2a3b20d79645f4c574eea7079fb9
Yannick <yannick.francillette1@uqac.ca>
2016-09-08 10:59:44 -0400

    Adds RFIDAntenna definitive version
    Adds RFIDAntenna definitive version

---
+++ Unity/Assets/Scripts/BehaviourTree/AbstractBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractBehaviour : MonoBehaviour {
+
+	public enum State {Running, Suceeded, Failed};
+
+	public abstract State Execute ();
+
+	private State behaviourState;
+
+	// Use this for initialization
+	void Start () {
+		behaviourState = State.Running;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		behaviourState = Execute ();
+	}
+
+}
---
+++ Unity/Assets/Scripts/BehaviourTree/AvatarBehaviours/MoveToBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToBehaviour : AbstractBehaviour {
+
+	private string roomToGo;
+	private string timeToGo;
+	private GameObject avatar;
+
+	public MoveToBehaviour (string roomToGo, string timeToGo, GameObject avatar)
+	{
+		this.roomToGo = roomToGo;
+		this.timeToGo = timeToGo;
+		this.avatar = avatar;
+	}
+
+	public override State Execute ()
+	{
+		if (IsTargetRoomReached ())
+			return State.Suceeded;
+		return State.Running;
+	}
+
+	private string GetCurrentRoom() {
+		return null;
+	}
+
+	private bool IsTargetRoomReached() {
+		return false;
+	}
+}
