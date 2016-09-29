473b8057c56ee3c174f0b64e449764d3a4db6c9b
eboucher4 <eric_2212@hotmail.ca>
2016-09-29 13:43:23 -0400

    New RFID

---
+++ Unity/Assets/ForceSignal.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ForceSignal : MonoBehaviour
+{
+    public string antennaID;
+    private SmartHomeServer smartHomeServer;
+    private List<ColliderData> dataList;
+
+    struct ColliderData
+    {
+        public float signal;
+        public GameObject collider;
+
+        public ColliderData(float a, GameObject b)
+        {
+            signal = a;
+            collider = b;
+        }
+    }
+
+    void Start()
+    {
+        dataList = new List<ColliderData>();
+    }
+
+    private void InitSmartHomeServerConnection()
+    {
+        GameObject smartHomeServerObject = GameObject.Find("smarthomeserver");
+        if (smartHomeServer == null)
+            Debug.Log("SmartHomeServer Not Loaded");
+        smartHomeServer = smartHomeServerObject.GetComponent<SmartHomeServer>();
+    }
+
+    private float CalculDistance(GameObject collider)
+    {
+        Vector3 positionCollider = collider.transform.position;
+        Vector3 positionCapteur = GetComponentInParent<Transform>().position;
+
+        return Vector3.Distance(positionCollider, positionCapteur);
+    }
+
+    private float CalculForceSignal(float distance)
+    {
+        float forceSignal = 0;
+
+        forceSignal = ((-9.1333f * Mathf.Log(distance)) - 10.726f);
+
+        return forceSignal;
+    }
+
+    private void StockData(float forceSignal, GameObject collider)
+    {
+        ColliderData colliderData = new ColliderData(forceSignal,collider);
+        dataList.Add(colliderData);
+    }
+
+    private void SendData()
+    {
+        string timestamp = System.DateTime.Now.ToLongTimeString();
+
+        if (smartHomeServer == null)
+            InitSmartHomeServerConnection();
+
+        else
+        {
+            for (int i = 0; i < dataList.Count; i++)
+                smartHomeServer.InsertRFIDData(timestamp, antennaID, dataList[i].signal, dataList[i].collider.name);
+        }
+    }
+
+    public void Desactivate()
+    {
+        SendData();
+        dataList.Clear();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        GameObject collider = other.gameObject;
+
+        float distance = 0;
+        float forceSignal = 0;
+
+        if (collider.tag == "RFID")
+        {
+            distance = CalculDistance(collider);
+            forceSignal = CalculForceSignal(distance);
+
+            StockData(forceSignal, collider);
+        }
+
+        else
+            return;
+    }
+};
---
+++ Unity/Assets/TokenRing.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TokenRing : MonoBehaviour {
+
+    public List<GameObject> listeCapteurs;
+    public float delay;
+    private int token;
+    private float activeTimer;
+    private bool activate;
+
+    void Start()
+    {
+        for (int i = 0; i < listeCapteurs.Count; i++)
+            listeCapteurs[i].gameObject.SetActive(false);
+
+        activate = false;
+        activeTimer = 0.25f;
+
+        token = listeCapteurs.Count - 1;
+    }
+
+    void Update()
+    {
+        if (CheckTimer() && activate == false)
+        {
+            listeCapteurs[token].transform.Find("Cone Detection").transform.gameObject.SetActive(true);
+
+            activate = true;
+            ResetTimer();
+        }
+
+        if (CheckTimer() && activate == true)
+        {
+            listeCapteurs[token].GetComponentInChildren<ForceSignal>().Desactivate();
+            listeCapteurs[token].GetComponentInChildren<GameObject>().SetActive(false);
+            GiveToken();
+
+            activate = false;
+            ResetTimer();
+        }
+    }
+
+   bool CheckTimer()
+    {
+        activeTimer = activeTimer - Time.deltaTime;
+
+        if (activeTimer <= 0)
+            return true;
+
+        else
+            return false;
+    }
+
+    void ResetTimer()
+    {
+        activeTimer = delay;
+    }
+
+
+    void GiveToken()
+    {
+        token = token - 1;
+
+        if (token == 0)
+            token = listeCapteurs.Count - 1;
+    }
+
+};
