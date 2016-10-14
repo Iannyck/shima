7db3e5484f8e7565671a4d247e4cc0f6e9f29e80
eboucher4 <eric_2212@hotmail.ca>
2016-10-14 11:27:34 -0400

    Drawer animation + GUI

---
+++ Unity/Assets/Scripts/UI_Choice.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Choice : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 90), "Choice Menu");
+
+        if (GUI.Button(new Rect(20, 40, 80, 20), "Choix 1"))
+        {
+            Debug.Log("Choix 1");
+        }
+
+        if (GUI.Button(new Rect(20, 70, 80, 20), "Choix 2"))
+        {
+            Debug.Log("Choix 2");
+        }
+    }
+}
