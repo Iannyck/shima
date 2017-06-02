851fba38b1b010d3f384ab3dd1a21a536959a79d
eboucher4 <eric_2212@hotmail.ca>
2017-06-02 15:14:36 -0400

    Sauvegarde et Chargement
    Permet de sauvegarder en Json les meubles et les charger.

--- Unity/Assets/Scripts/UI/EditManager.cs
+++ Unity/Assets/Scripts/UI/EditManager.cs
@@ -17,8 +17,9 @@ public class EditManager : MonoBehaviour {
 	public InputField thicknessNameInputField;
 
 	public GameObject editPanel;
+    public Furniture_Recepteur recepteur;
 
-	private GameObject GetClickedGameObject() {
+    private GameObject GetClickedGameObject() {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast (ray, out hit, Mathf.Infinity))
@@ -38,7 +39,8 @@ public class EditManager : MonoBehaviour {
 			GameObject target = GetClickedGameObject ();
 			if (target != null) {
 				selected = target;
-				mouseState = true;
+                recepteur = this.GetComponentInParent<BBuildManager>().FindInFurnitureList(selected);   // Permet de trouver la structure de données contenant l'entité ciblée
+                mouseState = true;
 				screenSpace = Camera.main.WorldToScreenPoint (selected.transform.position);
 				offset = selected.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
 				objectNameInputField.text = selected.name;
@@ -46,6 +48,7 @@ public class EditManager : MonoBehaviour {
 			} else {
 				mouseState = false;
 				selected = null;
+                recepteur = null; // Renitialise le recepteur a null
 				objectNameInputField.text = "Name";
 				angleNameInputField.text = "0";
 				editPanel.SetActive (false);
@@ -59,7 +62,11 @@ public class EditManager : MonoBehaviour {
 			Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenSpace) + offset;
 
 			selected.transform.position = curPosition;
-		}
+            
+            if (recepteur != null && selected != null)     
+                recepteur.UpdateInfos(selected);
+            
+        }
 //		if(Input.GetMouseButtonDown (0)) {
 //			mouseState = false;
 //			selected = null;
@@ -72,30 +79,48 @@ public class EditManager : MonoBehaviour {
 	public void RotateWall() {
 		Debug.Log (angleNameInputField.text);
 		if (selected != null) {
-			if(angleNameInputField.text != null && angleNameInputField.text != "")
-				selected.transform.Rotate (new Vector3 (0, float.Parse (angleNameInputField.text), 0));
+            if (angleNameInputField.text != null && angleNameInputField.text != "")
+            {
+                selected.transform.Rotate(new Vector3(0, float.Parse(angleNameInputField.text), 0));
+                recepteur.UpdateInfos(selected);
+            }
 		}
 	}
 
 	public void SetLenght() {
 		Debug.Log (angleNameInputField.text);
 		if (selected != null) {
-			if(lenghtNameInputField.text != null && lenghtNameInputField.text != "")
-				selected.transform.localScale = new Vector3 (float.Parse (lenghtNameInputField.text) * selected.transform.localScale.x, selected.transform.localScale.y, selected.transform.localScale.z);
+            if (lenghtNameInputField.text != null && lenghtNameInputField.text != "")
+            {
+                selected.transform.localScale = new Vector3(float.Parse(lenghtNameInputField.text) * selected.transform.localScale.x, selected.transform.localScale.y, selected.transform.localScale.z);
+                recepteur.UpdateInfos(selected);
+            }
+                
 		}
 	}
 
 	public void SetWidth() {
 		Debug.Log (angleNameInputField.text);
 		if (selected != null){
-			if(widthNameInputField.text != null && widthNameInputField.text != "")
-				selected.transform.localScale = new Vector3 (selected.transform.localScale.x, selected.transform.localScale.y, float.Parse (widthNameInputField.text) * selected.transform.localScale.z);
+            if (widthNameInputField.text != null && widthNameInputField.text != "")
+            {
+                selected.transform.localScale = new Vector3(selected.transform.localScale.x, selected.transform.localScale.y, float.Parse(widthNameInputField.text) * selected.transform.localScale.z);
+                recepteur.UpdateInfos(selected);
+            }
 		}
 	}
 
 	public void SetThickness() {
 		Debug.Log (thicknessNameInputField.text);
-	}
+        if (selected != null)
+        {
+            if (thicknessNameInputField != null && thicknessNameInputField.text != "")
+            {
+                // Fonction sur Thickness
+                recepteur.UpdateInfos(selected);
+            }
+        }
+    }
 
 //	public void RotateWall(Slider angleSlider) {
 //		Debug.Log (angleSlider.value);
