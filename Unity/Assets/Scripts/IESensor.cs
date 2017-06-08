3f9e9b0cd2f4437c4a039c6c036493bd8d724b82
Yannick <yannick.francillette1@uqac.ca>
2017-06-08 16:32:21 -0400

    new RFID + refact
    New RFID Scripts

---
+++ Unity/Assets/Scripts/IESensor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IESensor : MonoBehaviour {
+
+	public bool isSimulating = false;
+
+	private SmartHomeServer smartHomeServer;
+
+	public SmartHomeServer SmartHomeServer {
+		get {
+			return this.smartHomeServer;
+		}
+	}
+
+	private void InitSmartHomeServerConnection() {
+		GameObject smartHomeServerObject = GameObject.Find ("smarthomeserver");
+		if(smartHomeServerObject == null)
+			Debug.Log("SmartHomeServer Not Loaded");
+		else
+			smartHomeServer = smartHomeServerObject.GetComponent<SmartHomeServer> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (isSimulating)
+			InitSmartHomeServerConnection ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isSimulating) {
+			Sense (smartHomeServer);
+//			if (smartHomeServer == null)
+//				InitSmartHomeServerConnection ();
+//			else
+//				Sense (smartHomeServer);
+		}
+	}
+
+	/// <summary>
+	/// Sense the specified smartHomeServer.
+	/// Method to override to add specific sensing behavior
+	/// </summary>
+	/// <param name="smartHomeServer">Smart home server.</param>
+	protected virtual void Sense(SmartHomeServer smartHomeServer) {}
+}
---
+++ Unity/Assets/Scripts/RFID/RFIDSensor.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RFIDSensor : IESensor {
+
+	public float thresold = -71f; // -71 DB
+
+	public GameObject nextRFIDAntena;
+	public float activationTime = 0.25f;
+	private float remainingTime = 0f;
+
+	private GameObject detectionZone;
+	public bool isEnable = true;
+
+
+
+	public void RFIDActivate(bool value) {
+		if(value)
+			remainingTime = activationTime;
+		detectionZone.SetActive (value);
+		isEnable = value;
+	}
+
+	void Awake() {
+		RFIDDetectionZone rfidDetection = GetComponentInChildren<RFIDDetectionZone> ();
+		detectionZone = rfidDetection.gameObject;
+		remainingTime = activationTime;
+	}
+
+	protected override void Sense (SmartHomeServer smartHomeServer)
+	{
+		base.Sense (smartHomeServer);
+		if (isEnable) {
+			remainingTime -= Time.deltaTime;
+			if (remainingTime <= 0f) {
+				if (nextRFIDAntena != null) {
+					RFIDSensor rfidSensor = nextRFIDAntena.GetComponent<RFIDSensor> ();
+					rfidSensor.RFIDActivate (true);
+					RFIDActivate (false);
+				}
+			}
+		}
+	}
+
+	public void Trigger(GameObject collider) {
+		float rssi = RSSI (collider);
+		if (rssi >= thresold)
+			SmartHomeServer.InsertRFIDData(name, rssi, collider.name);
+	}
+
+	private float RSSI(GameObject collider) {
+		float distance = Vector3.Distance(this.transform.position, collider.transform.position);
+		float angle = Vector3.Angle(this.transform.position, collider.transform.position);
+		float angleLose = 1 - Mathf.Cos (angle);
+		float ambiantNoise = AmbiantNoise();
+		float obstacleLose = ObstacleNoise(collider);
+
+		float rssi = ((-9.1333f * Mathf.Log(distance)) - 10.726f) * angleLose;
+
+
+		return rssi;
+	}
+
+	public float AmbiantNoise() {
+		return 0f;
+	}
+
+	private float ObstacleNoise(GameObject collider) {
+		GameObject obstacle = GetObstacle (collider);
+		if (obstacle != null) {
+			return 0f;
+		}
+		return 0f;
+	}
+
+	public GameObject GetObstacle(GameObject collider)                                  
+	{
+		Transform sensorPosition = GetComponent<Transform>();
+		RaycastHit hitInfo;
+
+		Debug.DrawLine(sensorPosition.position, collider.transform.position, Color.red, 10f);
+
+		Physics.Linecast(sensorPosition.position, collider.transform.position, out hitInfo);
+		{
+			if (hitInfo.collider != null && (hitInfo.collider.CompareTag("Fixed") || hitInfo.collider.CompareTag("Door")))                             
+				return hitInfo.collider.gameObject;
+
+			else
+				return null;
+		}
+	}
+
+}
