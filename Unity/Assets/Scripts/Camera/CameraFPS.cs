4fd81019537b550b6010fc7a96364ca68bd8b527
Yannick <yannick.francillette1@uqac.ca>
2017-06-14 11:20:55 -0400

    Camera manager
    Camera manager

--- Unity/Assets/Scripts/Camera/CameraFPS.cs
+++ Unity/Assets/Scripts/Camera/CameraFPS.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class CameraFPS : MonoBehaviour {
 
+	public float cameraSensitivity = 90;
+	public float climbSpeed = 4;
+	public float normalMoveSpeed = 10;
+	public float slowMoveFactor = 0.25f;
+	public float fastMoveFactor = 3;
+
+	private float rotationX = 0.0f;
+	private float rotationY = 0.0f;
+
+	private Quaternion initRotation;
+
 	// Use this for initialization
 	void Start () {
-		
+		Screen.lockCursor = false;
+		initRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		rotationX += Input.GetAxis ("Mouse X") * cameraSensitivity * Time.deltaTime;
+		rotationX = Mathf.Clamp (rotationX, -90, 90);
+		rotationY += Input.GetAxis ("Mouse Y") * cameraSensitivity * Time.deltaTime;
+		rotationY = Mathf.Clamp (rotationY, -90, 90);
+
+		transform.localRotation = Quaternion.AngleAxis (rotationX, Vector3.up);
+		transform.localRotation *= Quaternion.AngleAxis (rotationY, Vector3.left);
+
+		if (Input.GetKeyDown (KeyCode.L)) {
+			Screen.lockCursor = !Screen.lockCursor;
+		}
+
 	}
 }
---
+++ Unity/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	[SerializeField] private List<GameObject> cameras;
+	[SerializeField] private GameObject current_camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.F1)) {
+			SwitchCamera (0);
+		} else if (Input.GetKey (KeyCode.F2)) {
+			SwitchCamera (1);
+		} else if (Input.GetKey (KeyCode.F3)) {
+			SwitchCamera (2);
+		}
+	}
+
+	private void SwitchCamera(int number) {
+		current_camera.SetActive (false);
+		current_camera = cameras [number];
+		current_camera.SetActive (true);
+	}
+}
